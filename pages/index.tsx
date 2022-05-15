98dc5287b66a4ca89b4e0d3e671fd47f46bda41b
Edgars Gaidels <edgars.gaidels@gmail.com>
2022-05-15 20:47:08 +0300

    Spotify API authentication added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,41 @@
-import Link from "next/link";
+import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
+import axios from "axios";
+import { CLIENT_ID, CLIENT_SECRET } from "../utils/credentials";
 
-const IndexPage = () => (
-	<Layout title="Evolution TypeScript bootcamp project">
-		<h1>Hi mom! 👋</h1>
-	</Layout>
-);
+const App = () => {
+	console.log("RENDERING APP...");
 
-export default IndexPage;
+	const [token, setToken] = useState("");
+	const [genres, setGenres] = useState([]);
+
+	useEffect(() => {
+		axios("https://accounts.spotify.com/api/token", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+			},
+			data: "grant_type=client_credentials",
+		}).then((tokenResponse) => {
+			setToken(tokenResponse.data.access_token);
+
+			axios("https://api.spotify.com/v1/browse/categories?locale=sv_US", {
+				method: "GET",
+				headers: { Authorization: "Bearer " + token },
+			}).then((genreResponse) => {
+				setGenres(genreResponse.data.categories.items);
+			});
+		});
+	}, []);
+
+	console.log(genres);
+
+	return (
+		<Layout>
+			<h1>Hi, Marco! 👋</h1>
+		</Layout>
+	);
+};
+
+export default App;
---
+++ utils/credentials.ts
@@ -0,0 +1,2 @@
+export const CLIENT_ID = "dc26f9c67f4540bb8d19b7b3a6c9c61e";
+export const CLIENT_SECRET = "6b5001f155fb4e3abd531e30483e1164";
