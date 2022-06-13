7d1377130e11b1776d3f3ed56ca498c0d18894ff
Edgars Gaidels <edgars.gaidels@gmail.com>
2022-06-13 18:14:08 +0300

    Refactored search box component

--- components/UI/SearchBox.tsx
+++ components/UI/SearchBox.tsx
@@ -5,23 +5,12 @@ import parse from "autosuggest-highlight/parse";
 import match from "autosuggest-highlight/match";
 import debounce from "lodash.debounce";
 import GenreChips from "./GenreChips";
+import { noArtist } from "utils/hooks/noArtist";
 
 const ARTISTS_ENDPOINT = "/api/artists";
 
-export interface IArtist {
-  name: string;
-  id: string;
-  genres: string[];
-}
-
-interface IArtists extends Array<IArtist> {}
-
-let artistData: SpotifyApi.ArtistSearchResponse;
-let artistList: IArtists = [{ name: "", id: "", genres: [""] }];
-const initialOptionList: IArtists = [];
-
 export default function SearchBox(): JSX.Element {
-  const [optionList, setOptionList] = useState<IArtists>(initialOptionList);
+  const [artistList, setArtistList] = useState<SpotifyApi.ArtistObjectFull[]>(noArtist);
   const [selectedArtist, setSelectedArtist] = useState<any>();
   const [genres, setGenres] = useState<string[] | null[] | undefined>([]);
 
@@ -29,43 +18,26 @@ export default function SearchBox(): JSX.Element {
     if (searchValue.length > 0) {
       await fetch(`${ARTISTS_ENDPOINT}/${searchValue}`)
         .then((response) => response.json())
-        .then((response) => {
-          return (artistData = response);
-        })
-        .finally(() => {
-          updateArtistList();
+        .then((response: SpotifyApi.ArtistSearchResponse) => {
+          setArtistList(response.artists.items.map((item) => item));
         })
         .catch((err) => console.log(err)); // eslint-disable-line no-console
     }
   }
 
-  function updateArtistList() {
-    artistList = artistData.artists.items.map((item) => {
-      return {
-        name: item.name,
-        id: item.id,
-        genres: item.genres,
-      };
-    });
-    setOptionList(artistList);
-  }
-
-  const inputHandler = (event: any, searchValue: string): void => {
-    if (!searchValue) {
-      setOptionList(initialOptionList);
-    } else {
-      callArtistsAPI(searchValue);
-    }
+  const inputHandler = (event: any, searchValue: string) => {
+    callArtistsAPI(searchValue);
   };
+
   const debouncedInputHandler = debounce(inputHandler, 500);
 
   const onChangeHandler = (event: any, newValue: any): void => {
     if (!newValue) {
-      setOptionList(initialOptionList);
+      setArtistList(noArtist);
       setGenres([null]);
     } else {
       setSelectedArtist(newValue);
-      setGenres(artistData?.artists.items.find((artist) => artist === artist)?.genres);
+      setGenres(artistList.find((artist) => artist === artist)?.genres);
     }
   };
 
@@ -75,7 +47,7 @@ export default function SearchBox(): JSX.Element {
         id="search-box"
         onInputChange={debouncedInputHandler}
         onChange={onChangeHandler}
-        options={optionList}
+        options={artistList}
         getOptionLabel={(option: any) => option.name}
         renderOption={(props, option, { inputValue }) => {
           const matches = match(option.name, inputValue);
---
+++ utils/hooks/noArtist.ts
@@ -0,0 +1,19 @@
+export const noArtist: SpotifyApi.ArtistObjectFull[] = [
+  {
+    external_urls: {
+      spotify: "",
+    },
+    followers: {
+      href: null,
+      total: 0,
+    },
+    genres: [],
+    href: "",
+    id: "none9146916491",
+    images: [],
+    name: "Keep on searching...",
+    popularity: 0,
+    type: "artist",
+    uri: "spotify:artist:none",
+  },
+];
